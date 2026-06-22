5433c1c58def04155062b55999486c6f3b2c346a
jfgjtdxxxxx <wparsa100@gmail.com>
2026-06-22 03:48:22 +0330

    Update settings.gradle.kts

--- V2rayNG/settings.gradle.kts
+++ V2rayNG/settings.gradle.kts
@@ -3,7 +3,7 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
-        maven { url = uri("https://jitpack.io") }
+        maven("https://jitpack.io")
     }
 }
 
@@ -12,7 +12,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
+        maven("https://jitpack.io")
     }
 }
 
