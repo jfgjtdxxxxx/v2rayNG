4b1e03460cd239d5a16f1d6c494b59a328e67c44
jfgjtdxxxxx <wparsa100@gmail.com>
2026-06-22 03:45:42 +0330

    Update build.gradle.kts

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -1,3 +1,9 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.serialization)
+}
+
 android {
     namespace = "com.v2ray.ang"
     compileSdk = 34
@@ -29,4 +35,46 @@ android {
         viewBinding = true
         dataBinding = true
     }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.swiperefreshlayout)
+    implementation(libs.recyclerview)
+    implementation(libs.preference.ktx)
+    implementation(libs.androidx.viewpager2)
+    implementation(libs.androidx.fragment)
+
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.runtime.ktx)
+
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.coroutines.core)
+
+    implementation(libs.okhttp)
+    implementation(libs.gson)
+    implementation(libs.core)
+
+    implementation(libs.mmkv.static)
+    implementation(libs.toasty)
+    implementation(libs.work.runtime.ktx)
+    implementation(libs.work.multiprocess)
+    implementation(libs.flexbox)
+    implementation(libs.quickie.foss)
+
+    implementation(libs.editorkit)
+    implementation(libs.language.base)
+    implementation(libs.language.json)
+
+    implementation(libs.multidex)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 }
